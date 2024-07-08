cc4d818a135884c6ba9b6f74d757c4e4da04af6f
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-08 13:38:38 +0200

    ISP

---
+++ SOLID/ISP.cs
@@ -0,0 +1,64 @@
+﻿namespace Altkom._8_10._07._2024.SOLID.I
+{
+    internal interface IExcelFormatter
+    {
+        void ToExcel();
+    }
+    internal interface IPdfFormatter
+    { 
+        void ToPdf();
+    }
+
+    class Report : IExcelFormatter, IPdfFormatter
+    {
+        public void ToExcel()
+        {
+            Console.WriteLine("Excel generated");
+        }
+
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+
+    class Poem : IPdfFormatter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+
+    /*internal interface IFormatter
+    {
+        void ToExcel();
+        void ToPdf();
+    }
+    
+    class Report : IFormatter
+    {
+        public void ToExcel()
+        {
+            Console.WriteLine("Excel generated");
+        }
+
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }
+
+    class Poem : IFormatter
+    {
+        public void ToExcel()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void ToPdf()
+        {
+            Console.WriteLine("Pdf generated");
+        }
+    }*/
+}
