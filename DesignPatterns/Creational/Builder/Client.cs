a34c4b5f212d8ca406c8f3b941d3a902d44790b0
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-08 14:33:47 +0200

    Builder: before

---
+++ DesignPatterns/Creational/Builder/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace Altkom._8_10._07._2024.DesignPatterns.Creational.Builder
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var vehicle = new Vehicle(4, 5, 4, 500, 100);
+
+            Console.WriteLine(vehicle);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,25 +2,12 @@
 using Altkom._8_10._07._2024.SOLID.L;
 
 
-int a = 4;
-int b = 9;
 
-
-var rectangle = CreateRectangle(a, b);
-
-
-Console.WriteLine($"{a} * {b} = {rectangle.Area}");
-
-
-Rectangle CreateRectangle(int a, int b)
-{
-    return new Square { A = a, B = b }; 
-}
+Altkom._8_10._07._2024.DesignPatterns.Creational.Builder.Client.Execute();
 
 
 
-
-static void DesignPrinciples()
+    static void DesignPrinciples()
 {
     var customerId = 1;
 
@@ -42,4 +29,22 @@ static void DesignPrinciples()
         Console.WriteLine($"Customer {customerId} charged. Actual balance: {customer.Account.GetBalance()}");
     else
         Console.WriteLine($"Not enought founds on customer {customerId} acconut! Actual balance: {customer.Account.GetBalance()}");
+}
+
+static void LSP()
+{
+    int a = 4;
+    int b = 9;
+
+
+    var rectangle = CreateRectangle(a, b);
+
+
+    Console.WriteLine($"{a} * {b} = {rectangle.Area}");
+
+
+    Rectangle CreateRectangle(int a, int b)
+    {
+        return new Square { A = a, B = b };
+    }
 }
