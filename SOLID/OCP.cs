24241c9045b798803199bcb2e3308e450501dfbd
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-08 13:09:42 +0200

    OCP

---
+++ SOLID/OCP.cs
@@ -0,0 +1,57 @@
+﻿namespace Altkom._8_10._07._2024.SOLID.O
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Square : Shape
+    {
+        public int A { get; set; }
+
+        public override double GetArea()
+        {
+            return A * A;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public override double GetArea()
+        {
+            return A * B;
+        }
+    }
+
+    class Circle : Shape
+    {
+        public int R { get; set; }
+
+        public override double GetArea()
+        {
+            return R * R * Math.PI;
+        }
+    }
+
+    class ShapeCalculator
+    {
+        double Area(Shape shape)
+        {
+            return shape.GetArea();
+            /*switch (shape)
+            {
+                case Square square:
+                    return square.A * square.A;
+                case Rectangle rectangle:
+                    return rectangle.A * rectangle.B;
+                case Circle circle:
+                    return circle.R * circle.R * Math.PI;
+                default:
+                    return 0;
+            }*/
+        }
+    }
+}
