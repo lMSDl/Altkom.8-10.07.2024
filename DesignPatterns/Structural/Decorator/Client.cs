7eb63429fbab1454ecb6fce35e386cf88f73e9bc
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-09 12:46:19 +0200

    Decorator: before

---
+++ DesignPatterns/Structural/Decorator/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._8_10._07._2024.DesignPatterns.Structural.Decorator
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            while (true)
+            {
+                var text = Console.ReadLine();
+                INotification notification = new SystemNotification();
+
+
+                notification.Send(text);
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Decorator/SystemNotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._8_10._07._2024.DesignPatterns.Structural.Decorator
+{
+    internal class SystemNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Wysłano notyfikację wewnętrzą: {message}");
+        }
+    }
+}
