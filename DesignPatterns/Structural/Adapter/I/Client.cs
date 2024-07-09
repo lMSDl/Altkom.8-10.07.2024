952d8d09cb66eac2940fd933d84ef89cb0852d6f
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-09 10:51:53 +0200

    Adapter: before

---
+++ DesignPatterns/Structural/Adapter/I/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace Altkom._8_10._07._2024.DesignPatterns.Structural.Adapter.I
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var circle = new Circle() { R = 5 };
+            CheckR(circle);
+        }
+
+        public static void CheckR(ICircle circle)
+        {
+            Console.WriteLine(circle.R <= 5);
+        }
+    }
+}
