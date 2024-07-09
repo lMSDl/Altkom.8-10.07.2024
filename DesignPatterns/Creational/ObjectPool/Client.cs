40d581df562351af131f1b26e9a15f8479d9579e
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-09 10:17:49 +0200

    ObjectPool: before

---
+++ DesignPatterns/Creational/ObjectPool/Client.cs
@@ -0,0 +1,21 @@
+﻿namespace Altkom._8_10._07._2024.DesignPatterns.Creational.ObjectPool
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var counter = 0;
+            while (true)
+            {
+                if (counter > 1000)
+                {
+                    Thread.Sleep(1);
+                    continue;
+                }
+                var item = new Item() { IsVisible = true };
+                Task.Delay(100).ContinueWith(x => { item.IsVisible = false; Interlocked.Decrement(ref counter); });
+                Interlocked.Increment(ref counter);
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Creational/ObjectPool/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace Altkom._8_10._07._2024.DesignPatterns.Creational.ObjectPool
+{
+    internal class Item
+    {
+        private bool isCollected;
+        public bool IsVisible { get; set; }
+        public bool IsCollected
+        {
+            get => isCollected;
+            set
+            {
+                if (isCollected = value)
+                    IsVisible = false;
+            }
+        }
+
+        public byte[] Content { get; } = new byte[1024];
+    }
+}
