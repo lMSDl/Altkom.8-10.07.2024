045b7586b9940cd3d30417b82af117d2dc6e9103
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-09 13:11:02 +0200

    Facade: before

---
+++ DesignPatterns/Structural/Facade/I/Client.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Altkom._8_10._07._2024.DesignPatterns.Structural.Facade.I
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var person = new Person() { LastName = "Personowski", Id = 12 };
+
+            var settings = new JsonSerializerOptions()
+            {
+                IgnoreReadOnlyProperties = true,
+                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault,
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
+
+
+            var json = JsonSerializer.Serialize(person, settings);
+            Console.WriteLine(json);
+        }
+
+    }
+}
---
+++ DesignPatterns/Structural/Facade/I/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._8_10._07._2024.DesignPatterns.Structural.Facade.I
+{
+    internal class Person
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Id { get; set; }
+
+        public int Age => DateTime.Now.Year - BirthDate.Year;
+    }
+}
