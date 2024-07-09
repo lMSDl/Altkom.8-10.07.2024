7e57c1c43c5e6068b75ee454d86aa13884794b18
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-09 10:05:16 +0200

    Singleton

--- DesignPatterns/Creational/Singleton/Context.cs
+++ DesignPatterns/Creational/Singleton/Context.cs
@@ -4,9 +4,18 @@
     {
         private readonly Dictionary<string, string> _settings = new Dictionary<string, string>();
 
-        public Context()
+        private Context()
         {
             Console.WriteLine("Inicjalizacja ustawień");
+
+            _settings.Add("1", "A");
+            _settings.Add("2", "B");
+            _settings.Add("3", "C");
+        }
+
+        private async Task Initialize()
+        {
+            await Task.Delay(1000);
 
             _settings.Add("1", "A");
             _settings.Add("2", "B");
@@ -21,5 +30,55 @@
         {
             _settings[key] = value;
         }
+
+        private static Context? _instance;
+
+        /*public static Context GetInstance()
+        {
+            if(_instance == null)
+                _instance = new Context();
+            return _instance;
+        }*/
+
+        private static readonly object _locker = new object();
+        /*public static Context GetInstance()
+        {
+            lock (_locker)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Context();
+                    //await _instance.Initialize();
+                }
+
+                return _instance;
+            }
+        }*/
+
+        /*public static Context GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_locker)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Context();
+                    }
+                }
+            }
+            return _instance;
+        }*/
+
+
+        static Context() { }
+        public static Context Instance { get; } = new Context();
+
+        /*private static Lazy<Context> _lazyInstance = new Lazy<Context>(() => new Context());
+        public static Context GetInstance()
+        {
+            return _lazyInstance.Value;
+        }*/
+
     }
 }
