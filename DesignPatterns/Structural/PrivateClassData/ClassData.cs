4793ea47ddf55f89e6e54f69e6a861a489e4e54a
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-09 15:30:00 +0200

    PrivateClassData

---
+++ DesignPatterns/Structural/PrivateClassData/ClassData.cs
@@ -0,0 +1,26 @@
+﻿namespace Altkom._8_10._07._2024.DesignPatterns.Structural.PrivateClassData
+{
+    internal class ClassData
+    {
+        //private readonly string _someString;
+        //private int SomeInt { get; }
+        private PrivateClassData _data;
+
+        public ClassData(string someString, int someInt)
+        {
+            //_someString = someString;
+            //SomeInt = someInt;
+            _data = new PrivateClassData(someString, someInt);
+        }
+
+
+        public void DoSth()
+        {
+            //_someString = "";
+            //SomeInt = 0;
+
+            _data.GetSomeString();
+            _data.GetSomeInt();
+        }
+    }
+}
---
+++ DesignPatterns/Structural/PrivateClassData/PrivateClassData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._8_10._07._2024.DesignPatterns.Structural.PrivateClassData
+{
+    internal class PrivateClassData
+    {
+        private string _someString;
+        private int _someInt;
+
+        public PrivateClassData(string someString, int someInt)
+        {
+            _someString = someString;
+            _someInt = someInt;
+        }
+
+        public int GetSomeInt()
+        {
+            return _someInt;
+        }
+
+        public string GetSomeString()
+        {
+            return _someString;
+        }
+    }
+}
