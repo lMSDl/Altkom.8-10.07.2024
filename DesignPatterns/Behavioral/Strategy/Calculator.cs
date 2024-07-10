33c38afa0f50e3b1f5708739f86e49eb873df1e7
Paweł Łęczycki <pawe@msd.info.pl>
2024-07-10 14:43:41 +0200

    Strategy

---
+++ DesignPatterns/Behavioral/Strategy/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._8_10._07._2024.DesignPatterns.Behavioral.Strategy
+{
+    internal class Calculator
+    {
+        public ICalcStrategy? Strategy { get; set; }
+        public float? Operate(float a, float b)
+        {
+            return Strategy?.Calc(a, b);
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Strategy/Client.cs
@@ -0,0 +1,60 @@
+﻿namespace Altkom._8_10._07._2024.DesignPatterns.Behavioral.Strategy
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var calculator = new Calculator();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var split = line.Split(' '); // 2 + 4
+                if (split.Length < 3)
+                    continue;
+
+                calculator.Strategy = GetStrategy(split[1]);
+
+                if (float.TryParse(split[0], out var a) && float.TryParse(split[2], out var b))
+                {
+                    Console.WriteLine(calculator.Operate(a, b));
+                    Console.WriteLine(GetFunc(split[1])(a, b));
+                }
+            }
+        }
+
+        private static ICalcStrategy? GetStrategy(string sign)
+        {
+            switch (sign)
+            {
+                case "+":
+                    return new PlusStrategy();
+                case "-":
+                    return new MinusStategy();
+                case "*":
+                    return new MultiplyStrategy();
+                case "/":
+                    return new DivideStrategy();
+                default:
+                    return null;
+            }
+        }
+
+        static Func<float, float, float> GetFunc(string sign)
+        {
+            switch (sign)
+            {
+                case "+":
+                    return (a, b) => a + b;
+                case "-":
+                    return (a, b) => a - b;
+                case "*":
+                    return (a, b) => a * b;
+                case "/":
+                    return (a, b) => a / b;
+                default:
+                    return null;
+            }
+        }
+    }
+}
